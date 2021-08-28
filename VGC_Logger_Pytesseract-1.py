ea1e5e5931db9528f4da188c248af8a48e48eecb
Flamerino <pokemongranada@gmail.com>
2021-08-28 21:33:08 +0200

    Final release 1.0

--- VGC_Logger_Pytesseract-1.py
+++ VGC_Logger_Pytesseract-1.py
@@ -1,12 +1,12 @@
-import os
 import device
 import numpy as np
 import cv2
 import pytesseract
 from multiprocessing import Process, Queue
+import time
 
-with open('Resources/OCR_Path.txt', "r") as file:
-    my_path = file.read()
+with open('Resources/OCR_Path.txt', "r") as pathfile:
+    my_path = pathfile.read()
 pytesseract.pytesseract.tesseract_cmd = str(my_path)
 
 
@@ -77,6 +77,7 @@ def checktextlength(myframe):
 
     return my_list
 
+
 def frame_ocr(q):
     my_file = None
     pokemon_name = "Void"
@@ -121,18 +122,18 @@ def get_hp(myframe):
             return round(porcentaje)
 
 
-def ventana(q, capturadora):
-
+def ventana(q, capture_card):
     # Colores
-    black_textbox_min = np.array([0, 0, 0])        # BLACK CUADRO TEXTO
-    black_textbot_max = np.array([60, 40, 120])    # BALCK CUADRO TEXTO
-    black_hp_box_min = np.array([0, 0, 0])         # BLACK HP BAR AND HP TEXT
-    black_hp_box_max = np.array([179, 255, 117])   # BALCK HP BAR AND HP TEXT
-    white_hp_box_min = np.array([0, 0, 145])       # WHITE CUADRO HP
-    white_hp_box_max = np.array([179, 69, 255])    # WHITE CUADRO HP
+    black_textbox_min = np.array([0, 0, 0])  # BLACK CUADRO TEXTO
+    black_textbot_max = np.array([60, 40, 120])  # BALCK CUADRO TEXTO
+    black_hp_box_min = np.array([0, 0, 0])  # BLACK HP BAR AND HP TEXT
+    black_hp_box_max = np.array([179, 255, 117])  # BALCK HP BAR AND HP TEXT
+    white_hp_box_min = np.array([0, 0, 145])  # WHITE CUADRO HP
+    white_hp_box_max = np.array([179, 69, 255])  # WHITE CUADRO HP
 
     # PROGRAMA
-    capture = cv2.VideoCapture(capturadora, cv2.CAP_DSHOW)
+    # capture = cv2.VideoCapture(capturadora, cv2.CAP_DSHOW)
+    capture = cv2.VideoCapture(capture_card)
     capture.set(3, 1280)
     capture.set(4, 720)
 
@@ -161,16 +162,13 @@ def ventana(q, capturadora):
     were_different4 = False
     printed_hp4 = None
 
-    i = 1
-    while True:
-        if os.path.exists("Logs/Log" + str(i) + '.txt'):
-            i += 1
-        else:
-            with open("Logs/Log" + str(i) + '.txt', "w+") as file:
-                file.write("THIS IS A BETA" + '\n')
-            q.put('FILE')
-            q.put("Logs/Log" + str(i) + '.txt')
-            break
+    timestr = time.strftime("%Y-%m-%d %H-%M-%S")
+    print('Log File name: ' + str(timestr) + '.txt')
+
+    with open("Logs/" + str(timestr) + '.txt', "w+") as logfile:
+        logfile.write("VGC LOGGER VERSION 1.0" + '\n')
+    q.put('FILE')
+    q.put("Logs/" + str(timestr) + '.txt')
 
     while True:
 
@@ -184,36 +182,36 @@ def ventana(q, capturadora):
         mask_hp_box_white = cv2.inRange(FrameHSV, white_hp_box_min, white_hp_box_max)
         mask_hp_box_black = cv2.inRange(FrameHSV, black_hp_box_min, black_hp_box_max)
 
-
         # PUNTOS DETECTION CUADRO TEXTO NEGRO
-        P1 = mask_textobox[610:620, 20:50]      # Pastilla negra top left
+        P1 = mask_textobox[610:620, 20:50]  # Pastilla negra top left
         P2 = mask_textobox[610:620, 1230:1260]  # Pastilla negra top right
-        P3 = mask_textobox[670:680, 10:30]      # Pastilla negra bot left
+        P3 = mask_textobox[670:680, 10:30]  # Pastilla negra bot left
         P4 = mask_textobox[610:620, 1230:1250]  # Pastilla negra bot right
-        P5 = mask_textobox[150:160, 100:200]    # Punto negativo pastilla negra
+        P5 = mask_textobox[150:160, 100:200]  # Punto negativo pastilla negra
         P6 = mask_textobox[150:160, 1100:1200]  # Punto negativo pastilla negra
 
         # PUNTOS DETECCION CUADRO HP1
-        P7 = mask_hp_box_white[620:693, 6:12]       # white HP1 left 438 (73x6)
-        P8 = mask_hp_box_white[620:693, 288:294]    # white HP1 right 438 (73x6)
-        P9 = mask_hp_box_white[600:610, 120:220]    # white HP1 Punto negativo 1000 (10x100)
+        P7 = mask_hp_box_white[620:693, 6:12]  # white HP1 left 438 (73x6)
+        P8 = mask_hp_box_white[620:693, 288:294]  # white HP1 right 438 (73x6)
+        P9 = mask_hp_box_white[600:610, 120:220]  # white HP1 Punto negativo 1000 (10x100)
 
         # PUNTOS DETECCION CUADRO HP2
-        P10 = mask_hp_box_white[644:693, 335:345]   # white HP2 left  490 (49*10)
-        P11 = mask_hp_box_white[620:693, 622:628]   # white HP2 right 438 (73x6)
-        P12 = mask_hp_box_white[600:610, 480:580]   # white HP2 punto negativo 1000 (10x100)
+        P10 = mask_hp_box_white[644:693, 335:345]  # white HP2 left  490 (49*10)
+        P11 = mask_hp_box_white[620:693, 622:628]  # white HP2 right 438 (73x6)
+        P12 = mask_hp_box_white[600:610, 480:580]  # white HP2 punto negativo 1000 (10x100)
 
         # PUNTOS DETECCION CUADRO HP3
-        P13 = mask_hp_box_white[25:80, 627:633]     # white HP3 left 330
-        P14 = mask_hp_box_white[25:80, 915:921]     # white HP3 right 330
-        P15 = mask_hp_box_white[90:100, 750:850]    # white HP3 punto negativo 1000
+        P13 = mask_hp_box_white[25:80, 627:633]  # white HP3 left 330
+        P14 = mask_hp_box_white[25:80, 915:921]  # white HP3 right 330
+        P15 = mask_hp_box_white[90:100, 750:850]  # white HP3 punto negativo 1000
 
         # PUNTOS DETECCION CUADRO HP4
-        P16 = mask_hp_box_white[25:80, 969:975]     # white HP4 left 330
-        P17 = mask_hp_box_white[25:80, 1260:1266]   # white HP4 right 330
+        P16 = mask_hp_box_white[25:80, 969:975]  # white HP4 left 330
+        P17 = mask_hp_box_white[25:80, 1260:1266]  # white HP4 right 330
         P18 = mask_hp_box_white[90:100, 1100:1200]  # white HP4 punto negativo 1000
 
-        if checkpoint(P1, 250) and checkpoint(P2, 250) and checkpoint(P3, 150) and checkpoint(P4, 150) and checkpoint(P5, 950) != True and checkpoint(P6, 950) != True:
+        if checkpoint(P1, 250) and checkpoint(P2, 250) and checkpoint(P3, 150) and checkpoint(P4, 150) and checkpoint(
+                P5, 950) is not True and checkpoint(P6, 950) is not True:
             if text_read is False:
                 current_length = checktextlength(mask_textobox)
                 if previous_length == current_length:
@@ -233,10 +231,10 @@ def ventana(q, capturadora):
             # print("FINALIZADO LECTURA TEXTO")
 
         # GET HP1
-        if checkpoint(P7, 350) and checkpoint(P8, 350) and checkpoint(P9, 590) != True:
+        if checkpoint(P7, 350) and checkpoint(P8, 350) and checkpoint(P9, 590) is not True:
             HP1 = mask_hp_box_black[658:662, 16:281]
             current_hp1 = get_hp(HP1)
-            if current_hp1 == previous_hp1 and were_different1 == True:
+            if current_hp1 == previous_hp1 and were_different1 is True:
                 hp1_counter += 1
             elif current_hp1 != previous_hp1:
                 hp1_counter = 0
@@ -245,7 +243,7 @@ def ventana(q, capturadora):
 
             if hp1_counter == 6:
                 if printed_hp1 != current_hp1:
-                    slot_name1 = mask_hp_box_black[620:650, 10:160]   # 30 x 150
+                    slot_name1 = mask_hp_box_black[620:650, 10:160]  # 30 x 150
                     slot_name1 = cv2.bitwise_not(slot_name1)
                     # cv2.imshow("cuadro", slot_name1)
                     q.put(slot_name1)
@@ -253,12 +251,11 @@ def ventana(q, capturadora):
                     printed_hp1 = current_hp1
                     were_different1 = False
                     hp1_counter = 0
-
-       # get HP2
-        if checkpoint(P10, 350) and checkpoint(P11, 350) and checkpoint(P12, 590) != True:
+        # get HP2
+        if checkpoint(P10, 350) and checkpoint(P11, 350) and checkpoint(P12, 590) is not True:
             HP2 = mask_hp_box_black[658:662, 350:615]
             current_hp2 = get_hp(HP2)
-            if current_hp2 == previous_hp2 and were_different2 == True:
+            if current_hp2 == previous_hp2 and were_different2 is True:
                 hp2_counter += 1
             elif current_hp2 != previous_hp2:
                 hp2_counter = 0
@@ -277,10 +274,10 @@ def ventana(q, capturadora):
                     hp2_counter = 0
 
         # get HP3
-        if checkpoint(P13, 265) and checkpoint(P14, 265) and checkpoint(P15, 590) != True:
+        if checkpoint(P13, 265) and checkpoint(P14, 265) and checkpoint(P15, 590) is not True:
             HP3 = mask_hp_box_black[66:70, 638:903]
             current_hp3 = get_hp(HP3)
-            if current_hp3 == previous_hp3 and were_different3 == True:
+            if current_hp3 == previous_hp3 and were_different3 is True:
                 hp3_counter += 1
             elif current_hp3 != previous_hp3:
                 hp3_counter = 0
@@ -299,10 +296,10 @@ def ventana(q, capturadora):
                     hp3_counter = 0
 
         # get HP4
-        if checkpoint(P16, 265) and checkpoint(P17, 265) and checkpoint(P18, 590) != True:
+        if checkpoint(P16, 265) and checkpoint(P17, 265) and checkpoint(P18, 590) is not True:
             HP4 = mask_hp_box_black[66:70, 980:1245]
             current_hp4 = get_hp(HP4)
-            if current_hp4 == previous_hp4 and were_different4 == True:
+            if current_hp4 == previous_hp4 and were_different4 is True:
                 hp4_counter += 1
             elif current_hp4 != previous_hp4:
                 hp4_counter = 0
